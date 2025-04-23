a9fb81bb7662a33873cfc1696617760f5e98da78
nweellyyyy <94314413+nweellyyyy@users.noreply.github.com>
2025-04-24 02:21:26 +0800

    Create library.py

---
+++ Front-End/library.py
@@ -0,0 +1,463 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+
+    <STYle>.library {
+        position: relative;
+        width: 1440px;
+        height: 1024px;
+        background-color: #ffffff;
+      }
+      
+      .library .frame {
+        position: absolute;
+        width: 296px;
+        height: 968px;
+        top: 28px;
+        left: 22px;
+        border-radius: 25px;
+        overflow: hidden;
+        background: linear-gradient(
+          180deg,
+          rgba(0, 44, 105, 1) 0%,
+          rgba(111, 75, 188, 1) 68%,
+          rgba(75, 175, 252, 1) 98%
+        );
+      }
+      
+      .library .dashboard {
+        position: absolute;
+        width: 173px;
+        height: 48px;
+        top: 340px;
+        left: 29px;
+      }
+      
+      .library .text-wrapper {
+        position: absolute;
+        top: 11px;
+        left: 54px;
+        font-family: "DM Sans-Regular", Helvetica;
+        font-weight: 400;
+        color: #ffffff;
+        font-size: 23px;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .dashboard-icon {
+        position: absolute;
+        width: 48px;
+        height: 48px;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+      }
+      
+      .library .div {
+        position: absolute;
+        width: 125px;
+        height: 39px;
+        top: 398px;
+        left: 35px;
+      }
+      
+      .library .library-icon {
+        position: absolute;
+        width: 39px;
+        height: 39px;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+      }
+      
+      .library .text-wrapper-2 {
+        position: absolute;
+        top: 9px;
+        left: 48px;
+        font-family: "DM Sans-Regular", Helvetica;
+        font-weight: 400;
+        color: #ffffff;
+        font-size: 23px;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .help {
+        position: absolute;
+        width: 101px;
+        height: 43px;
+        top: 456px;
+        left: 33px;
+      }
+      
+      .library .text-wrapper-3 {
+        position: absolute;
+        top: 7px;
+        left: 50px;
+        font-family: "DM Sans-Regular", Helvetica;
+        font-weight: 400;
+        color: #ffffff;
+        font-size: 23px;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .img {
+        position: absolute;
+        width: 43px;
+        height: 43px;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+      }
+      
+      .library .overlap-group {
+        position: absolute;
+        width: 330px;
+        height: 142px;
+        top: 40px;
+        left: -17px;
+      }
+      
+      .library .colored {
+        position: absolute;
+        width: 111px;
+        height: 111px;
+        top: 0;
+        left: 109px;
+        object-fit: cover;
+      }
+      
+      .library .text-wrapper-4 {
+        position: absolute;
+        width: 330px;
+        top: 100px;
+        left: 0;
+        font-family: "DM Sans-Bold", Helvetica;
+        font-weight: 700;
+        color: #ffffff;
+        font-size: 30px;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .group {
+        position: absolute;
+        width: 145px;
+        height: 42px;
+        top: 1254px;
+        left: 47px;
+      }
+      
+      .library .text-wrapper-5 {
+        left: 41px;
+        font-size: 32px;
+        position: absolute;
+        top: 0;
+        font-family: "DM Sans-Regular", Helvetica;
+        font-weight: 400;
+        color: #ffffff;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .untitled-design {
+        position: absolute;
+        width: 32px;
+        height: 32px;
+        top: -2463px;
+        left: -22px;
+        object-fit: cover;
+      }
+      
+      .library .group-2 {
+        position: absolute;
+        width: 103px;
+        height: 30px;
+        top: 911px;
+        left: 52px;
+      }
+      
+      .library .text-wrapper-6 {
+        left: 28px;
+        font-size: 23px;
+        position: absolute;
+        top: 0;
+        font-family: "DM Sans-Regular", Helvetica;
+        font-weight: 400;
+        color: #ffffff;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .untitled-design-2 {
+        position: absolute;
+        width: 21px;
+        height: 21px;
+        top: 4px;
+        left: 0;
+        object-fit: cover;
+      }
+      
+      .library .about {
+        position: absolute;
+        width: 120px;
+        height: 43px;
+        top: 282px;
+        left: 31px;
+      }
+      
+      .library .text-wrapper-7 {
+        position: absolute;
+        top: 7px;
+        left: 52px;
+        font-family: "DM Sans-Regular", Helvetica;
+        font-weight: 400;
+        color: #ffffff;
+        font-size: 23px;
+        text-align: center;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .overlap {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 160px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .rectangle {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 0;
+        left: 0;
+        background-color: #d9d9d9cc;
+        border-radius: 10px;
+      }
+      
+      .library .text-wrapper-8 {
+        top: 13px;
+        white-space: nowrap;
+        position: absolute;
+        width: 260px;
+        left: 26px;
+        font-family: "Karla-Light", Helvetica;
+        font-weight: 300;
+        color: #000000;
+        font-size: 30px;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .overlap-2 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 243px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .overlap-3 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 326px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .overlap-4 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 409px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .overlap-5 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 492px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .overlap-6 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 575px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .overlap-7 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 658px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .overlap-8 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 741px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .text-wrapper-9 {
+        top: 13px;
+        position: absolute;
+        width: 260px;
+        left: 26px;
+        font-family: "Karla-Light", Helvetica;
+        font-weight: 300;
+        color: #000000;
+        font-size: 30px;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .overlap-9 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 824px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .text-wrapper-10 {
+        top: 12px;
+        position: absolute;
+        width: 260px;
+        left: 26px;
+        font-family: "Karla-Light", Helvetica;
+        font-weight: 300;
+        color: #000000;
+        font-size: 30px;
+        letter-spacing: 0;
+        line-height: normal;
+      }
+      
+      .library .overlap-10 {
+        position: absolute;
+        width: 1009px;
+        height: 62px;
+        top: 907px;
+        left: 368px;
+        border-radius: 10px;
+      }
+      
+      .library .text-wrapper-11 {
+        position: absolute;
+        width: 201px;
+        top: 61px;
+        left: 368px;
+        font-family: "Karla-ExtraBold", Helvetica;
+        font-weight: 800;
+        color: #000000;
+        font-size: 55px;
+        letter-spacing: 0;
+        line-height: normal;
+        white-space: nowrap;
+      }
+      </STYle>
+  </head>
+  <body>
+    <div class="library">
+      <div class="frame">
+        <div class="dashboard">
+          <div class="text-wrapper">Dashboard</div>
+          <img class="dashboard-icon" src="img/dashboard-icon-1.png" />
+        </div>
+        <div class="div">
+          <img class="library-icon" src="img/library-icon-1.png" />
+          <div class="text-wrapper-2">Library</div>
+        </div>
+        <div class="help">
+          <div class="text-wrapper-3">Help</div>
+          <img class="img" src="img/help-icon-1.png" />
+        </div>
+        <div class="overlap-group">
+          <img class="colored" src="img/colored-1.png" />
+          <div class="text-wrapper-4">BEACompanion</div>
+        </div>
+        <div class="group">
+          <div class="text-wrapper-5">Logout</div>
+          <img class="untitled-design" src="img/untitled-design-9-1.png" />
+        </div>
+        <div class="group-2">
+          <div class="text-wrapper-6">Logout</div>
+          <img class="untitled-design-2" src="img/logout.png" />
+        </div>
+        <div class="about">
+          <div class="text-wrapper-7">About</div>
+          <img class="img" src="img/about-icon-1.png" />
+        </div>
+      </div>
+      <div class="overlap">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 1</div>
+      </div>
+      <div class="overlap-2">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 2</div>
+      </div>
+      <div class="overlap-3">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 3</div>
+      </div>
+      <div class="overlap-4">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 4</div>
+      </div>
+      <div class="overlap-5">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 5</div>
+      </div>
+      <div class="overlap-6">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 6</div>
+      </div>
+      <div class="overlap-7">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-8">Patient 7</div>
+      </div>
+      <div class="overlap-8">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-9">Patient 8</div>
+      </div>
+      <div class="overlap-9">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-10">Patient 9</div>
+      </div>
+      <div class="overlap-10">
+        <div class="rectangle"></div>
+        <div class="text-wrapper-10">Patient 10</div>
+      </div>
+      <div class="text-wrapper-11">Library</div>
+    </div>
+  </body>
+</html>
