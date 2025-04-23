ad7d0097948b7a1b8dc579d6f705ad3c837a2df0
nweellyyyy <94314413+nweellyyyy@users.noreply.github.com>
2025-04-24 02:21:05 +0800

    Create index.py

---
+++ Front-End/index.py
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="login">
+      <div class="overlap">
+        <div class="frame">
+          <div class="overlap-group">
+         
+            <p class="understand">
+              <span class="text-wrapper">Understand behavioral and speech patterns </span>
+              <span class="span">like never before!</span>
+              <span class="text-wrapper"></span>Gain deep insights into ADHD assessment through intelligent monitoring and analysis—helping children
+                thrive with confidence.</span
+              >
+            </p>
+          </div>
+          <div class="welcome-to">Welcome to <br />BEACompanion!</div>
+        </div>
+        <img class="psychiatrist-for" src="img/psychiatrist-for-child 1.png" />
+        <div class="rectangle"></div>
+        <div class="div"></div>
+        <div class="text-wrapper-2">Password</div>
+        <div class="text-wrapper-3">Email</div>
+        <div class="rectangle-2"></div>
+        <div class="text-wrapper-4">Login to continue</div>
+        <div class="text-wrapper-5">Welcome Back!</div>
+        <div class="group">
+          <div class="div-wrapper"><div class="text-wrapper-6">Login</div></div>
+        </div>
+        <div class="text-wrapper-7">New User?</div>
+        <div class="text-wrapper-8">Sign Up</div>
+      </div>
+    </div>
+  </body>
+</html>
